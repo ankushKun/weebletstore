ae8a86dce4f3f43331aa8a93bec71dbee322d166
ankushKun <ankush4singh@gmail.com>
2023-11-26 20:30:58 +0530

    finally migrated

--- sandbox/msanity.js
+++ sandbox/msanity.js
@@ -2,37 +2,37 @@ import data from "./newdata.json" assert { type: "json" }
 import fs from "fs"
 
 fs.unlinkSync("migrated.ndjson")
-
+const slugs = []
 
 function migrate(item) {
+    id++
+    let slug = item.name && item.name.toLowerCase().replace(/ /g, "-")
+    if (slugs.includes(slug)) {
+        console.log("Duplicate slug: " + slug)
+        slug = slug + "-" + parseInt(id).toString()
+    }
+    slugs.push(slug)
     return {
-        _id: parseInt(id++).toString(),
+        _id: parseInt(id).toString(),
         _type: "items",
         name: item.name,
         slug: {
             _type: "slug",
-            current: item.name && item.name.toLowerCase().replace(/ /g, "-"),
+            current: slug,
         },
         anime: item.anime,
         itype: item.itype,
         price: item.price,
-        // images: item.images && item.images.map(image => "image@" + image),
-        images: {
-            _type: "array",
-            of: {
-                _type: "image",
-                asset: {
-                    _type: "image",
-                    _sanityAsset: "image@" + item.images[0],
-                },
-            }
-        },
+        images: [{
+            _type: "image",
+            _sanityAsset: "image@" + item.images[0],
+        }],
         postDescription: item.postDescription,
         available: true,
     }
 }
 
-let id = 1
+let id = 0
 Object.keys(data.items).forEach(key => {
     console.log(key)
     const migrated = migrate(data.items[key])

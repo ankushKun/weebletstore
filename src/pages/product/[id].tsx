8304e94ee7de21f3bdbe0df76fa1f7e21a2c9ff8
ankushKun <ankush4singh@gmail.com>
2023-11-27 02:04:36 +0530

    search working

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -5,15 +5,17 @@ import Image from "next/image";
 import { Item } from "@/types";
 import { urlFor } from "@/utils/sanity/client";
 
-// export async function getServerSideProps({ query }: { query: any }) {
-//   const base =
-//     process.env.ENV === "DEV" ? process.env.DEV_URL : process.env.PROD_URL;
-//   const item = await fetch(base + `/api/item-details?id=${query.id}`).then(
-//     (res) => res.json(),
-//   );
+export async function getServerSideProps({ query }: { query: any }) {
+  console.log(query);
+  // const base =
+  //   process.env.ENV === "DEV" ? process.env.DEV_URL : process.env.PROD_URL;
+  // const item = await fetch(base + `/api/item-details?id=${query.id}`).then(
+  //   (res) => res.json(),
+  // );
 
-//   return { props: { item } };
-// }
+  // return { props: { item } };
+  return { props: { item: {} } };
+}
 
 export default function Product({ item }: { item: Item }) {
   const [imagePos, setImagePos] = useState(0);

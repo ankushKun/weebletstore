2d4d9d9cc468623a8fd935fc39ef0ac087e97496
ankushKun <ankush4singh@gmail.com>
2023-11-28 10:41:39 +0530

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,36 @@
-import Link from "next/link"
-import { Button } from "@mantine/core"
+import Link from "next/link";
+import { Button } from "@mantine/core";
+import { IconBrandInstagram } from "@tabler/icons-react";
 
 export default function Footer() {
-    return <div className="bg-black w-full p-10 mt-5">
-        <div className="text-center mb-5">© weebletstore.in 2023</div>
-        <div className="w-full flex items-center justify-center gap-4">
-            <Link href="/about"><Button>About</Button></Link>
-            <Link href="/contact"><Button>Contact Us</Button></Link>
+  return (
+    <div className="mt-5 flex w-full flex-col justify-around gap-6 bg-black p-10 md:flex-row">
+      <div>
+        <Link
+          href="https://instagram.com/weeblets_anime_store"
+          target="_blank"
+          className="flex items-center justify-center gap-2"
+        >
+          <IconBrandInstagram size={30} />
+          @weeblets_anime_store
+        </Link>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="text-center">© weebletstore.in 2023</div>
+        <div className="flex w-full items-center justify-center gap-4">
+          <Link href="/about">
+            <Button>About</Button>
+          </Link>
+          <Link href="/contact">
+            <Button>Contact Us</Button>
+          </Link>
         </div>
+      </div>
+      <div className="flex flex-col gap-2 text-center">
+        <Link href="/tnc">Terms & Conditions</Link>
+        <Link href="/privacy">Privacy Policy</Link>
+        <Link href="/privacy">Shipping Policy</Link>
+      </div>
     </div>
+  );
 }
